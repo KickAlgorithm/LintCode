f11a80241dc7d090b8ce7967f3965155ee612f34
Louis1992 <303542123@qq.com>
2016-06-09 11:04:25 -0700

    Maximum Product Subarray

---
+++ Maximum Product Subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int maxEndHere = nums[0];
+        int minEndHere = nums[0];
+        int maxSoFar = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            int num = nums[i];
+            if (num >= 0) {
+                maxEndHere = max(maxEndHere * num, num);
+                minEndHere = min(minEndHere * num, num);
+            } else {
+                int temp = maxEndHere;
+                maxEndHere = max(minEndHere * num, num);
+                minEndHere = min(temp * num, num);
+            }
+            maxSoFar = max(maxEndHere, maxSoFar);
+        }
+        return maxSoFar;
+    }
+};
